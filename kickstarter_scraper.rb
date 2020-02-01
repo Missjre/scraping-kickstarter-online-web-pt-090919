c122ce3911403d03f324959ed1faad4cd1c41437
Jalysa Reason <jalysareason@gmail.com>
2020-02-01 13:42:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,5 +2,9 @@ require "nokogirl"
 require "pry"
 
 def create_project_hash
- 
+ html = File.read('fixtures/kickstarter.html')
+ kickstarter = Nokogiri::HTML(html)
+ binding.pry
 end
+
+create_project_hash
